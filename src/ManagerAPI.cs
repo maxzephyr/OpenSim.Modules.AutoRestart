89e04b05d5798ec084f07103da82442054ee2aa0
Christopher <christopher.latza@gmail.com>
2018-01-04 01:21:20 +0100

    add debug messages

--- src/ManagerAPI.cs
+++ src/ManagerAPI.cs
@@ -1,13 +1,17 @@
-﻿using System;
+﻿using log4net;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Net;
+using System.Reflection;
 using System.Text;
 
 namespace OpenSim.Modules.AutoRestart
 {
     class ManagerAPI
     {
+        private static readonly ILog m_log = LogManager.GetLogger(MethodBase.GetCurrentMethod().DeclaringType);
+
         private static WebClient m_client = null;
 
         private static void createClient()
@@ -26,10 +30,12 @@ namespace OpenSim.Modules.AutoRestart
 
                 String _adress = "http://" + new Uri(_URL).Authority + "/opensim/restart";
                 String _data = "pass=" + _authPass.Trim() + "&trigger=" + _trigger.Trim() + "&uuid=" + _regionID.Trim() + "";
+                m_log.Info("[ManagerAPI] Send Restart Command (URL: " + _adress + ") (Data: " + _data + ")");
                 m_client.UploadStringAsync(new Uri(_adress), _data);
-            }catch
+            }
+            catch (Exception _error)
             {
-
+                m_log.Error("[ManagerAPI] sendRestartCommand: " + _error.Message);
             }
         }
 
@@ -41,11 +47,12 @@ namespace OpenSim.Modules.AutoRestart
 
                 String _adress = "http://" + new Uri(_URL).Authority + "/opensim/stop";
                 String _data = "pass=" + _authPass.Trim() + "&trigger=" + _trigger.Trim() + "&uuid=" + _regionID.Trim() + "";
+                m_log.Info("[ManagerAPI] Send ShutDown Command (URL: " + _adress + ") (Data: " + _data + ")");
                 m_client.UploadStringAsync(new Uri(_adress), _data);
             }
-            catch
+            catch(Exception _error)
             {
-
+                m_log.Error("[ManagerAPI] sendShutDownCommand: " + _error.Message);
             }
         }
     }
